128f25e8152c73aabf14960f9532cc3cef514df4
davidjarrett <david.jarrett@cube.global>
2022-12-30 14:52:16 +0000

    initial commit

--- Cost_Calculator.py
+++ Cost_Calculator.py
@@ -247,8 +247,8 @@ if authentication_status:
         amount_by_month_bills_4.groupby(by=["Item"],as_index=False).sum(["Amount"])
     )
     amount_by_month_bills_5_adj = pd.DataFrame(columns=['Item','Adj_Amount'])
-    amount_by_month_bills_5_adj['Item'] = ['Council Tax','TV Licence','Water','Road Fund','Internet','Gas & Electricity','Building & Contents Insurance','Amazon Prime','BT Sport']
-    amount_by_month_bills_5_adj['Adj_Amount'] = [1980.36,92.75,161.64,0,145.76,493.41,489.39,98.75,175]
+    amount_by_month_bills_5_adj['Item'] = ['Council Tax','TV Licence','Water','Road Fund','Internet','Gas & Electricity','Building & Contents Insurance','Amazon Prime','BT Sport','Ring']
+    amount_by_month_bills_5_adj['Adj_Amount'] = [1980.36,92.75,161.64,0,145.76,493.41,489.39,98.75,175,49.98]
     amount_by_month_bills_6 = amount_by_month_bills_5.merge(amount_by_month_bills_5_adj, on='Item', how='outer')
     amount_by_month_bills_6['Overall Amount'] = amount_by_month_bills_6['Adj_Amount'].fillna(0) + amount_by_month_bills_6['Amount'].fillna(0)
     amount_by_month_bills_7 = amount_by_month_bills_6[['Item','Overall Amount']].sort_values(by=['Overall Amount'], ascending=False)
@@ -616,13 +616,13 @@ if authentication_status:
         left_column, right_column, mid_column, last = st.columns(4)
         with left_column:
             st.text("Credit Card Debt:")
-            st.subheader(f"£10,160.60")
+            st.subheader(f"£10,250")
         with right_column:
             st.text("Savings:")
-            st.subheader(f"£35,000")
+            st.subheader(f"£4,000")
         with mid_column:
             st.text("Capital in Mortgages:")
-            st.subheader(f"£473,000")
+            st.subheader(f"£476,000")
         with last:
             st.text("Joint Salary")
             st.subheader(f"£130,000")
