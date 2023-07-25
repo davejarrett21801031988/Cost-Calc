db4575ce5e8a973be1179d9fd6b5aabe239fc281
David Jarrett <david.jarrett@apollounderwriting.com>
2023-07-25 10:14:48 +0100

    initial commit

--- Cost_Calculator.py
+++ Cost_Calculator.py
@@ -501,7 +501,7 @@ if authentication_status:
     cars_1['Purchase Date'] = [date(2019,1,26),date(2019,2,1),date(2016,12,13),date(2015,6,13),date(2013,9,14)]
     cars_1['Today'] = [date.today(),date.today(),date(2019,1,26),date(2019,2,1),date(2016,12,13)]
     cars_1['Registered Days'] = cars_1['Today'] - cars_1['Registered Date']
-#    cars_1['Registered Days'] = cars_1['Registered Days'].dt.days
+    cars_1['Registered Days'] = cars_1['Registered Days'].dt.days
 #    print(cars_1['Registered Days'])
 #       if not pd.api.types.is_datetime64_any_dtype(cars_1['Registered Days']):
 #    cars_1['Registered Days'] = pd.to_datetime(cars_1['Registered Days'])
