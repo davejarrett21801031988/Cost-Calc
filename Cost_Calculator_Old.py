d68d473ef3a7013c2e06d5d532df6287c739e4c5
David Jarrett <david.jarrett@apollounderwriting.com>
2023-08-02 09:40:17 +0100

    initial commit

--- Cost_Calculator_Old.py
+++ Cost_Calculator_Old.py
@@ -100,6 +100,12 @@ if authentication_status:
 
     #df3.info()
 
+    per = df3["Period"].unique()
+    print(per)
+    cat = df3["Category"].unique()
+    print(cat)
+    Uniq = xxx.merge(amount_by_month_pre, on='Period', how='left')
+
     # ---Sidebar---
     authenticator.logout("Logout", "sidebar")
     st.sidebar.title(f"Welcome {name}")
@@ -192,7 +198,14 @@ if authentication_status:
         #showlegend=False,
     )
 
+    #df3_periods = (
+    #    df3.groupby(by=["Period"],as_index=False)
+    #)
+    #print(df3_periods)
+
+
     amount_by_month = Spend_df[["Period","Category","Amount"]]
+    #amount_by_month = xxx.merge(amount_by_month_pre, on='Period', how='left')
 
     amount_by_month_grouped = (
         amount_by_month.groupby(by=["Period","Category"],as_index=False).sum(["Amount"]).sort_values(by=["Period"])
@@ -261,6 +274,7 @@ if authentication_status:
     amount_by_month_bills_6['Overall Amount'] = amount_by_month_bills_6['Adj_Amount'].fillna(0) + amount_by_month_bills_6['Amount'].fillna(0)
     amount_by_month_bills_7 = amount_by_month_bills_6[['Item','Overall Amount']].sort_values(by=['Overall Amount'], ascending=False)
     #print(amount_by_month_bills_6)
+
     months_done = (periodcalc_5 + 7)
     month_phrase = str(months_done) + str(' complete Months')
     amount_by_month_bills_7['Monthly Cost'] = amount_by_month_bills_7['Overall Amount'] / months_done
@@ -289,7 +303,6 @@ if authentication_status:
     spend_rate_3 = spend_rate_3.fillna(0)
     spend_rate_3["Average Monthly Spend"] = spend_rate_3["Average Spend"]
     spend_rate_4 = spend_rate_3.loc[:, ['Category','Average Monthly Spend','Spend this Month','Spend Rate']]
-    spend_rate_4['Average Monthly Spend'] = spend_rate_4['Average Monthly Spend'].map('£{:,.2f}'.format)
     spend_rate_4['Spend this Month'] = spend_rate_4['Spend this Month'].map('£{:,.2f}'.format)
     spend_rate_4['Spend Rate'] = spend_rate_4['Spend Rate']*100
     if spend_rate_4.empty:
@@ -348,12 +361,63 @@ if authentication_status:
     average_by_month_99 = average_by_month_99[~average_by_month_99["Period"].isin(optionsperiods)]
     average_by_month_99 = average_by_month_99[~average_by_month_99["Category"].isin(options77)]
     #print(average_by_month_99)
+
+    #Bill_Cat = ['House Bills']
+    #average_by_bill = Spend_df[["Period","Amount","Category",'Item']]
+    #average_by_bill = average_by_bill[~average_by_bill["Period"].isin(this_period)]
+    #average_by_bill = average_by_bill[~average_by_bill["Period"].isin(optionsperiods)]
+    #average_by_bill = average_by_bill[average_by_bill["Category"].isin(Bill_Cat)]
+    ##print(average_by_bill)
+    #average_by_month_grouped_99_Bill = (
+    #    average_by_bill.groupby(by=["Period","Item"],as_index=False).sum(["Amount"]).sort_values(by=["Period"])
+    #)
+    ##print(average_by_month_grouped_99_Bill)
+
+    ###average total spend by period
     average_by_month_grouped_99 = (
         average_by_month_99.groupby(by=["Period"],as_index=False).sum(["Amount"]).sort_values(by=["Period"])
     )
     #print(average_by_month_grouped_99)
     average_by_month_grouped_99_Previous = average_by_month_grouped_99[~average_by_month_grouped_99["Period"].isin(previous_period)]
     #print(average_by_month_grouped_99_Previous)
+
+    ###average spend by category by period
+    average_by_month_grouped_99_Cat = (
+        average_by_month_99.groupby(by=["Period","Category"],as_index=False).sum(["Amount"]).sort_values(by=["Period"])
+    )
+    #print(average_by_month_grouped_99_Cat)
+    average_by_month_grouped_99_Previous_Cat = average_by_month_grouped_99_Cat[~average_by_month_grouped_99_Cat["Period"].isin(previous_period)]
+    #print(average_by_month_grouped_99_Previous_Cat)
+    average_by_month_grouped_99_Cat_2 = (
+        average_by_month_grouped_99_Previous_Cat.groupby(by=["Category"],as_index=False).mean(["Amount"]).sort_values(by=["Category"])
+    )
+    #print(average_by_month_grouped_99_Cat_2)
+    spend_rate_6 = spend_rate_5.merge(average_by_month_grouped_99_Cat_2, on='Category', how='left')
+
+    spend_rate_6['Change Fig'] = spend_rate_6['Average Monthly Spend'] - spend_rate_6['Amount']
+    spend_rate_6['Change'] = (spend_rate_6['Change Fig'] / spend_rate_6['Amount']) * 100
+    #spend_rate_6['Amount'] = spend_rate_6['Amount'].map('£{:,.2f}'.format)
+    spend_rate_6['Average Monthly Spend'] = spend_rate_6['Average Monthly Spend'].map('£{:,.2f}'.format)
+    spend_rate_6['Average Monthly Spend Change'] = spend_rate_6['Change'].map('{:,.1f}%'.format)
+    spend_rate_7 = spend_rate_6[['Category','Average Monthly Spend','Average Monthly Spend Change','Spend this Month','Spend Rate','Status']]
+    #print(spend_rate_7)
+
+    ###average spend by house bill by period
+    #average_by_month_grouped_99_Bill_2 = average_by_month_grouped_99_Bill[~average_by_month_grouped_99_Bill["Period"].isin(previous_period)]
+    ##print(average_by_month_grouped_99_Bill_2)
+    #average_by_month_grouped_99_Prev_Bill = (
+    #    average_by_month_grouped_99_Bill_2.groupby(by=["Item"],as_index=False).sum(["Amount"]).sort_values(by=["Item"])
+    #)
+    #print(average_by_month_grouped_99_Prev_Bill)
+    #spend_rate_6 = spend_rate_5.merge(average_by_month_grouped_99_Cat_2, on='Category', how='left')
+
+    #spend_rate_6['Change Fig'] = spend_rate_6['Average Monthly Spend'] - spend_rate_6['Amount']
+    #spend_rate_6['Change'] = (spend_rate_6['Change Fig'] / spend_rate_6['Amount']) * 100
+    ##spend_rate_6['Amount'] = spend_rate_6['Amount'].map('£{:,.2f}'.format)
+    #spend_rate_6['Average Monthly Spend'] = spend_rate_6['Average Monthly Spend'].map('£{:,.2f}'.format)
+    #spend_rate_6['Average Monthly Spend Change'] = spend_rate_6['Change'].map('{:,.1f}%'.format)
+    #spend_rate_7 = spend_rate_6[['Category','Average Monthly Spend','Average Monthly Spend Change','Spend this Month','Spend Rate','Status']]
+    ##print(spend_rate_7)
 
     if average_by_month_grouped_99.empty:
         Average_spend = "£0.00"
@@ -608,7 +672,7 @@ if authentication_status:
     # --- mainpage ---
     selected = option_menu(
         menu_title = None,
-        options=["Overview","Add Data","Modify Data","Cars","Mortgage Calculator"],
+        options=["Overview","Add Data","Modify Data","Cars","Mortgage Calculator","Training Plan"],
         orientation = "horizontal"
     )
 
@@ -688,7 +752,7 @@ if authentication_status:
         st.subheader("Spend Rate for the Month")
         st.text("With the current rate of spending in the Month, are we predicted to go over/under the Average Monthly Spend?")
         st.text(day_phrase)
-        st.table(spend_rate_5)
+        st.table(spend_rate_7)
         st.markdown("---")
 
         st.subheader("House Bills")
@@ -1198,3 +1262,105 @@ if authentication_status:
             st.plotly_chart(fig_pie_2)
 
         st.table(schedule)
+
+    if selected == "Training Plan":
+
+        st.markdown(f"<div id='linkto_{0}'></div>", unsafe_allow_html=True)
+        st.title(":runner: Training Plan")
+        st.markdown("##")
+
+        #fixed_rate_mortgage(interest, years, payments_year, loan, start_date)
+
+        # CSS to inject contained in a string
+        hide_table_row_index = """
+                    <style>
+                    tbody th {display:none}
+                    .blank {display:none}
+                    </style>
+                    """
+
+        # Inject CSS with Markdown
+        st.markdown(hide_table_row_index, unsafe_allow_html=True)
+
+        col_1, col_2, col_3, col_4 = st.columns(4)
+        with col_1:
+            fn_Hours = st.text_input("Maximum Hours per Week", value = 10, key = "Hours")
+        with col_2:
+            fn_Weeks = st.text_input("Weeks to Race", value = 10, key = "Weeks")
+        with col_3:
+            fn_Race = st.text_input("Race", value = 'Marathon', key = "Race")
+        with col_4:
+            fn_Target = st.text_input("Target Time", value = '02:29:59', key = "Target")
+        st.markdown("##")
+
+        Target_Pace = int(150/42.2) + (int((((150/42.2)-int(150/42.2))*60))/100)
+        #day_phrase = str('Day ')+str(this_day)+str(' of ')+str(daysinmonth)
+        Easy_Pace = Target_Pace + 1
+        Speed_Pace = Target_Pace - 0.25
+
+        first, left_column, right_column, mid_column, last = st.columns(5)
+        with first:
+            st.text("Target Race Pace:")
+            st.subheader(f"{Target_Pace:,.2f}")
+        with left_column:
+            st.text("Easy Pace:")
+            st.subheader(f"{Easy_Pace:,.2f}")
+        with right_column:
+            st.text("Speed Work Pace (1km):")
+            st.subheader(f"{Speed_Pace:,.2f}")
+        with mid_column:
+            st.text("X:")
+            #st.subheader(f"{(Total_Original_Payments - Total_Payments)/12:,.1f}")
+        with last:
+            st.text("Y:")
+            #st.subheader(f"{Total_Payments/12:,.1f}")
+        st.markdown("##")
+
+        Biggest_Week = int(0.75 * int(fn_Weeks))
+        #BW_RunningDistance = (BW_EasyRunning*4.5) + (BW_Speedwork*3.5)
+
+        df_t = pd.DataFrame(columns=['Week', 'Easy Running', 'Speedwork', 'Cross Training', 'Running Distance', 'Running Time', 'Total Time', 'Index', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun', 'Total'])
+        df_t['Week'] = list(range(1,int(fn_Weeks)+1))
+        df_t['Index'] = df_t['Week'] - Biggest_Week
+        df_t['Index'] = abs(df_t['Index'])
+        df_t['Total Time'] = int(fn_Hours) * (1 - (df_t['Index'] * 0.07))
+        df_t['Running Time'] = 0.7 * df_t['Total Time']
+        df_t['Easy Running'] = 0.8 * df_t['Running Time']
+        df_t['Speedwork'] = df_t['Running Time'] - df_t['Easy Running']
+        df_t['Cross Training'] = df_t['Total Time'] - df_t['Running Time']
+        df_t['Running Distance'] = (df_t['Easy Running'] * (60 / Easy_Pace)) + (df_t['Speedwork'] * (60 / (Speed_Pace + 0.25)))
+        df_t['Mon X'] = (df_t['Cross Training'] * 0.33) * 60
+        df_t['Wed X'] = (df_t['Cross Training'] * 0.33) * 60
+        df_t['Fri X'] = (df_t['Cross Training'] * 0.33) * 60
+        df_t['Sun'] = (df_t['Easy Running'] * (60 / Easy_Pace)) * 0.4
+        df_t['Wed'] = (df_t['Easy Running'] * (60 / Easy_Pace)) * 0.18
+        df_t['Fri'] = (df_t['Easy Running'] * (60 / Easy_Pace)) * 0.14
+        df_t['Mon'] = (df_t['Easy Running'] * (60 / Easy_Pace)) * 0.08
+        df_t['Tue Easy'] = ((df_t['Easy Running'] * (60 / Easy_Pace)) * 0.05)
+        df_t['Tue Fast'] = ((df_t['Speedwork'] * (60 / (Speed_Pace + 0.25))) * 0.25)
+        df_t['Thu Easy'] = ((df_t['Easy Running'] * (60 / Easy_Pace)) * 0.07)
+        df_t['Thu Fast'] = ((df_t['Speedwork'] * (60 / (Speed_Pace + 0.25))) * 0.35)
+        df_t['Sat Easy'] = ((df_t['Easy Running'] * (60 / Easy_Pace)) * 0.07)
+        df_t['Sat Fast'] = ((df_t['Speedwork'] * (60 / (Speed_Pace + 0.25))) * 0.40)
+        df_t['Total'] = df_t['Sun'] + df_t['Wed'] + df_t['Fri'] + df_t['Mon'] + df_t['Tue'] + df_t['Thu'] + df_t['Sat'] - df_t['Running Distance']
+        df_t['Mon'] = df_t['Mon'].map('{:,.1f}km'.format) + ' + ' + df_t['Mon X'].map('{:,.0f}mins XTraining'.format)
+        df_t['Tue'] = df_t['Tue Easy'].map('{:,.1f}km Easy'.format) + ' + ' + df_t['Tue Fast'].map('{:,.1f}km Fast'.format)
+        df_t['Wed'] = df_t['Wed'].map('{:,.1f}km'.format) + ' + ' + df_t['Wed X'].map('{:,.0f}mins XTraining'.format)
+        df_t['Thu'] = df_t['Thu Easy'].map('{:,.1f}km Easy'.format) + ' + ' + df_t['Thu Fast'].map('{:,.1f}km Fast'.format)
+        df_t['Fri'] = df_t['Fri'].map('{:,.1f}km'.format) + ' + ' + df_t['Fri X'].map('{:,.0f}mins XTraining'.format)
+        df_t['Sat'] = df_t['Sat Easy'].map('{:,.1f}km Easy'.format) + ' + ' + df_t['Sat Fast'].map('{:,.1f}km Fast'.format)
+        df_t['Sun'] = df_t['Sun'].map('{:,.1f}km'.format)
+        df_t['Running Distance'] = df_t['Running Distance'].map('{:,.1f}km'.format)
+        df_t['Total Time'] = df_t['Total Time'].map('{:,.1f}hrs'.format)
+
+        #st.subheader("Spend Rate for the Month")
+        #st.text("With the current rate of spending in the Month, are we predicted to go over/under the Average Monthly Spend?")
+        #st.text(day_phrase)
+        st.table(df_t[['Week', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun', 'Running Distance', 'Total Time']])
+        st.markdown("---")
+
+        #print(Biggest_Week)
+        #print(BW_TotalRunning)
+        #print(BW_EasyRunning)
+        #print(BW_Speedwork)
+        #print(BW_XTraining)
