b975641a2cf2ccea8ada26be4d3a641d9eef145b
David Jarrett <dj.218@hotmail.co.uk>
2023-09-15 14:25:31 +0100

    initial commit

--- Cost_Calculator.py
+++ Cost_Calculator.py
@@ -418,7 +418,7 @@ if authentication_status:
         Average_spend_Previous = average_by_month_grouped_99_Previous["Amount"].mean()
 
         #print(Average_spend
-        #print(Average_spend_Previous)
+        #print(Average_spend_Previous
 
         df_selection_cap = df3.query(
             "Period == @Month_Sidebar & Category == 'Mortgage Capital'"
